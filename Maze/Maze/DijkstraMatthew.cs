1da473db86102eef837b1cbe2383632f7c3fc500
Matthew Burgess <burgessm@uwplatt.edu>
2019-03-26 17:26:30 -0500

    DijkstraMatthew.cs was added to the project, and shells for the abstract methods in the Search class were added.  All functionality is pending.

---
+++ Maze/Maze/DijkstraMatthew.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maze
+{
+    class DijkstraMatthew:Search
+    {
+        public DijkstraMatthew():base()
+        {
+
+        }
+
+        public override void SetupSearch(AINode[,] nodeMap, int startX, int startY, int endX, int endY)
+        {
+            
+        }
+
+        public override float RunSearch()
+        {
+            return 0;
+        }
+
+        public override bool RunRealTimeTick(int times, out List<AINode> nodesSearched)
+        {
+            nodesSearched = new List<AINode>();
+            return false;
+        }
+
+
+    }
+}
